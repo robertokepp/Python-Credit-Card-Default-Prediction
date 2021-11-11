2834f41cb4d639de48356f4499ba2040eb1b8ef5
robertokepp <robertokepp@hotmail.com>
2021-11-11 11:28:37 -0400

    se agrego para leer por consola

--- TecnologiasEmergentes.py
+++ TecnologiasEmergentes.py
@@ -23,12 +23,12 @@ warnings.filterwarnings("ignore")
 # MARRIAGE: estado marital (1=casado , 2=soltero, 3=otros)
 # AGE: edad
 # (-1=pago debidamente , 1=demora en el pago por 1 meses , 2=demora en el pago por 2 meses , 3=demora en el pago por 3 meses , 4=demora en el pago por 4 meses , 5=demora en el pago por 5 meses , 6=demora en el pago por 6 meses , 7=demora en el pago por 7 meses , 8=demora en el pago por 8 meses , 9=demora en el pago por 9 meses o mayor)
-# PAY_0: estado de reembolso en septiembre
-# PAY_1: estado de reembolso en agosto
-# PAY_2: estado de reembolso en julio
-# PAY_3: estado de reembolso en junio
-# PAY_4: estado de reembolso en mayo
-# PAY_5: estado de reembolso en abril
+# PAY_1: estado de reembolso en septiembre
+# PAY_2: estado de reembolso en agosto
+# PAY_3: estado de reembolso en julio
+# PAY_4: estado de reembolso en junio
+# PAY_5: estado de reembolso en mayo
+# PAY_6: estado de reembolso en abril
 # BILL_AMT1: monto del estado de cuenta en septiembre
 # BILL_AMT2: monto del estado de cuenta en agosto
 # BILL_AMT3: monto del estado de cuenta en julio
@@ -43,14 +43,48 @@ warnings.filterwarnings("ignore")
 # PAY_AMT6: monto del pago anterior abril
 # default.payment.next.month: pago por defecto (1=si , 0=no)
 
+# Ingreso de datos
+limit_bal = input("limit_bal: ")
+sex = input("sex (1=masculino , 2=femenino): ")
+education = input("education (1=postgrado , 2=universidad , 3=escuela secundaria , 4=otros , 5=desconocido , 6=desconocido)): ")
+marriage = input("marriage (1=casado , 2=soltero, 3=otros): ")
+age = input("age: ")
+
+print("\n(-1=pago debidamente , 1=demora en el pago por 1 meses , 2=demora en el pago por 2 meses , 3=demora en el pago por 3 meses , 4=demora en el pago por 4 meses , 5=demora en el pago por 5 meses , 6=demora en el pago por 6 meses , 7=demora en el pago por 7 meses , 8=demora en el pago por 8 meses , 9=demora en el pago por 9 meses o mayor)")
+
+pay_1 = input("pay_1 (estado de reembolso en septiembre): ")
+pay_2 = input("pay_2 (estado de reembolso en agosto): ")
+pay_3 = input("pay_3 (estado de reembolso en julio): ")
+pay_4 = input("pay_4 (estado de reembolso en junio): ")
+pay_5 = input("pay_5 (estado de reembolso en mayo): ")
+pay_6 = input("pay_6 (estado de reembolso en abril): ")
+
+print("\n(0-∞ = ammount)")
+
+bill_amt1 = input("bill_amt1 (monto del estado de cuenta en septiembre): ")
+bill_amt2 = input("bill_amt2 (monto del estado de cuenta en agosto): ")
+bill_amt3 = input("bill_amt3 (monto del estado de cuenta en julio): ")
+bill_amt4 = input("bill_amt4 (monto del estado de cuenta en junio): ")
+bill_amt5 = input("bill_amt5 (monto del estado de cuenta en mayo): ")
+bill_amt6 = input("bill_amt6 (monto del estado de cuenta en abril): ")
+
+print("\n(0-∞ = ammount)")
+
+pay_amt1 = input("pay_amt1 (monto del pago anterior septiembre): ")
+pay_amt2 = input("pay_amt2 (monto del pago anterior agosto): ")
+pay_amt3 = input("pay_amt3 (monto del pago anterior julio): ")
+pay_amt4 = input("pay_amt4 (monto del pago anterior junio): ")
+pay_amt5 = input("pay_amt5 (monto del pago anterior mayo): ")
+pay_amt6 = input("pay_amt6 (monto del pago anterior abril): ")
+
 # Variables
 names = []
 models = []
 results = []
-wait = 10
+wait = 0
 probability = 0.90
 
-# funciones
+# Funciones
 def confusionMatrix(confusionMatrixResult, labelsIndex=['Actual: YES','Actual: NO'], labelsColumns=['Predicted: YES','Predicted: NO']):
     df = pd.DataFrame(data=confusionMatrixResult, index=labelsIndex, columns=labelsColumns)
     df.loc['Total'] = df.sum()
@@ -86,7 +120,7 @@ models.append(('Logistic Regression', LogisticRegression()))
 models.append(('Decision Tree Classifier', DecisionTreeClassifier()))
 models.append(('Naive Bayes Classifier', GaussianNB()))
 
-print('Models')
+print('\nModels')
 # Loop de los modelos
 for name, model in models:
 
@@ -184,12 +218,13 @@ print(modelResults*100)
 time.sleep(wait)
 
 # Data de la nueva prediccion
-newPredictionDataDefault = OrderedDict([('limit_bal', 10000),('sex', 1 ),('education', 1 ),('marriage', 1 ),('age', 30 ),('pay_0', 9 ),('pay_1', 9 ),('pay_2', 9 ),('pay_3', 9),('pay_4', 9), ('pay_5', 0),('bill_amt1', 10),('bill_amt2', 20 ),('bill_amt3', 30 ),('bill_amt4', 40 ),('bill_amt5', 50 ),('bill_amt6', 60 ), ('pay_amt1', 100 ),('pay_amt2', 200 ),('pay_amt3', 300 ),('pay_amt4', 400 ),('pay_amt5', 500 ), ('pay_amt6', 600 )])
-newPredictionDataPay = OrderedDict([('limit_bal', 20000),('sex', 2 ),('education', 2 ),('marriage', 2 ),('age', 30 ),('pay_0', -1 ),('pay_1', -1 ),('pay_2', -1 ),('pay_3', -1),('pay_4', -1), ('pay_5', 0),('bill_amt1', 100),('bill_amt2', 200 ),('bill_amt3', 300 ),('bill_amt4', 400 ),('bill_amt5', 500 ),('bill_amt6', 600 ), ('pay_amt1', 1000 ),('pay_amt2', 2000 ),('pay_amt3', 3000 ),('pay_amt4', 4000 ),('pay_amt5', 5000 ), ('pay_amt6', 6000 )])
-
+newPredictionDataDefault = OrderedDict([('limit_bal', 10000),('sex', 1),('education', 1),('marriage', 1),('age', 30),('pay_1', 9),('pay_2', 9),('pay_3', 9),('pay_4', 9),('pay_5', 9), ('pay_6', 0),('bill_amt1', 10),('bill_amt2', 20 ),('bill_amt3', 30 ),('bill_amt4', 40 ),('bill_amt5', 50 ),('bill_amt6', 60 ), ('pay_amt1', 100 ),('pay_amt2', 200 ),('pay_amt3', 300 ),('pay_amt4', 400 ),('pay_amt5', 500 ), ('pay_amt6', 600 )])
+newPredictionDataPay = OrderedDict([('limit_bal', 20000),('sex', 2),('education', 2),('marriage', 2),('age', 30),('pay_1', -1),('pay_2', -1),('pay_3', -1),('pay_4', -1),('pay_5', -1), ('pay_6', 0),('bill_amt1', 100),('bill_amt2', 200 ),('bill_amt3', 300 ),('bill_amt4', 400 ),('bill_amt5', 500 ),('bill_amt6', 600 ), ('pay_amt1', 1000 ),('pay_amt2', 2000 ),('pay_amt3', 3000 ),('pay_amt4', 4000 ),('pay_amt5', 5000 ), ('pay_amt6', 6000 )])
+newPredictionDataEntry= OrderedDict([('limit_bal', limit_bal),('sex', sex),('education', education),('marriage', marriage),('age', age),('pay_1', pay_1),('pay_2', pay_2),('pay_3', pay_3),('pay_4', pay_4),('pay_5', pay_5), ('pay_6', pay_6),('bill_amt1', bill_amt1),('bill_amt2', bill_amt2 ),('bill_amt3', bill_amt3 ),('bill_amt4', bill_amt4 ),('bill_amt5', bill_amt5 ),('bill_amt6', bill_amt6 ), ('pay_amt1', pay_amt1 ),('pay_amt2', pay_amt2 ),('pay_amt3', pay_amt3 ),('pay_amt4', pay_amt4 ),('pay_amt5', pay_amt5 ), ('pay_amt6', pay_amt6 )])
 # Nueva prediccion
 newPredictionDefault = pd.Series(newPredictionDataDefault)
 newPredictionPay = pd.Series(newPredictionDataPay)
+newPredictionEntry = pd.Series(newPredictionDataEntry)
 
 # Se imprime la data
 print("\n\n\nNew Prediction Data")
@@ -197,12 +232,21 @@ print("\nNew Prediction Data Default")
 print(newPredictionDataDefault)
 print("\nNew Prediction Data Pay")
 print(newPredictionDataPay)
+print("\nNew Prediction Entry")
+print(newPredictionDataEntry)
 
 # Espera de n segundos
 time.sleep(wait)
 
 # Se imprime el resultado
 print("\n\n\nNew Prediction Result")
+print("\nNew Prediction Default")
 print(individualPrediction(newPredictionDefault))
+print("\nNew Prediction Pay")
 print(individualPrediction(newPredictionPay))
+print("\nNew Prediction Entry")
+print(individualPrediction(newPredictionEntry))
 print("\n")
+
+# Espera de 60 segundos
+time.sleep(60)
